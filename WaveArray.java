6221d3f242ba82f0adb8ea8a97d9d87d1037f0b3
Alka Yadav <yalka834@gmail.com>
2025-08-25 10:01:48 +0000

    WaveArray

---
+++ WaveArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class WaveArray{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter elements of array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n-1;i=i+2){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+         for(int i=0;i<n;i++){
+             System.out.print(arr[i]+" ");
+         }
+    }
+}
+
+        
